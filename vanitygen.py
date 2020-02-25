4a37de7adc1575db37e9c6196d88e1890df5fb94
frstrtr <frstrtr@gmail.com>
2020-02-25 17:17:29 +0300

    wif and uncompressed pub key added

--- vanitygen.py
+++ vanitygen.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #    coineva vanitygen.py
-#    Copyright (C) 2016 February 
+#    Copyright (C) 2016 February
 #    1200 Web Development
 #    http://1200wd.com/
 #
@@ -20,44 +20,99 @@
 #
 
 import os
-from bitcoin import *
+from bitcoin import encode_privkey, fast_multiply, pubkey_to_address, G
 import timeit
 import random
 import multiprocessing
+import binascii # importing binascii to be able to convert hexadecimal strings to binary data
+import hashlib
+import base58
+import ecdsa
 
 
-def address_search(pipeout, search_for='12o'):
+def hex2wif(hex_key):
+
+    # Step 1: here we have the private key
+    # private_kic = hex_key
+    # Step 2: let's add 80 in front of it
+    extended_key = "80"+hex_key
+    # Step 3: first SHA-256
+    first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+    # Step 4: second SHA-256
+    second_sha256 = hashlib.sha256(
+        binascii.unhexlify(first_sha256)).hexdigest()
+    # Step 5-6: add checksum to end of extended key
+    final_key = extended_key+second_sha256[:8]
+    # Step 7: finally the Wallet Import Format is the base 58 encode of final_key
+    WIF = base58.b58encode(binascii.unhexlify(final_key))
+    return WIF
+
+
+def key_data(privkey):
+    priv_key = encode_privkey(privkey, 'hex')
+    # PRIVATE! SIGNING KEY ECDSA.SECP256k1
+    sk = ecdsa.SigningKey.from_string(bytearray.fromhex(
+        priv_key), curve=ecdsa.SECP256k1)
+    # PUBLIC! VERIFYING KEY (64 BYTE LONG, MISSING 04 BYTE AT THE BEGINNING)
+    vk = sk.verifying_key
+    # FULL PUBLIC KEY
+    # add 04 byte at the beginning
+    pub_key = str(binascii.hexlify(b'\04'+vk.to_string()).decode())
+    pid = os.getpid()
+    wif_key = str(hex2wif(priv_key).decode())
+    return priv_key, pub_key, pid, wif_key
+
+
+def key_data_output(private_key, public_key, pid, wif_key, search_for, address, count, start):
+    f = open('keys.txt', 'a')
+    f.write("Searching for %s\n" % (search_for))
+    f.write("Searched %d addresses in %d seconds (pid %d)\n" %
+            (count, timeit.default_timer()-start, os.getpid()))
+    f.write("Found address: %s\n" % address)
+    f.write("This is my Uncompressed Public Key:\n%s\n" % public_key)
+    f.write("Priv key HEX 80 Byte: 80%s\n" % private_key)
+    f.write("WIF key: %s\n\n" % wif_key)
+    f.close()
+
+    # PRINTOUT KEYs DATA
+    print("Searching for %s (pid %s)" % (search_for, pid))
+    print("Searched %d in %d seconds (pid %d)" %
+          (count, timeit.default_timer()-start, os.getpid()))
+    print("Found address %s" % address)
+    print("This is my Uncompressed Public Key:\n %s" % public_key)
+    print("Priv key HEX 80 Byte: %s" % '80'+private_key)
+    print("WIF key: %s" % wif_key)
+
+
+def address_search(search_for='1Love'):
     privkey = random.randrange(2**256)
     address = ''
     count = 0
     start = timeit.default_timer()
 
-    os.write(pipeout, "Searching for %s (pid %s)" % (search_for, os.getpid()))
-
+    print("Searching for %s (pid %s)" % (search_for, os.getpid()))
     while not search_for in address:
         privkey += 1
         pubkey_point = fast_multiply(G, privkey)
         address = pubkey_to_address(pubkey_point)
         count += 1
         if not count % 1000:
-            os.write(pipeout, "Searched %d in %d seconds (pid %d)" % (count, timeit.default_timer()-start, os.getpid()))
+            print("Searched %d in %d seconds (pid %d)" %
+                  (count, timeit.default_timer()-start, os.getpid()))
 
-    os.write(pipeout, "Found address %s" % address)
-    os.write(pipeout, "Private key HEX %s" % encode_privkey(privkey,'hex'))
+    private_key, public_key, pid, wif_key = key_data(privkey)
+    key_data_output(private_key, public_key, pid,
+                    wif_key, search_for, address, count, start)
 
-def main():
-    # processors = multiprocessing.cpu_count()
-    # processors = 2
-    # print("You have %d processors so starting %d threads" % (processors, processors))
-    # for i in range(processors):
 
+def thread():
     pipein, pipeout = os.pipe()
     pid = os.fork()
     if pid == 0:
         os.close(pipein)
         address_search(pipeout)
     else:
-        # pipein = os.fdopen(pipein)
+        pipein = os.fdopen(pipein)
         while True:
             line = os.read(pipein, 32)
             print(line)
@@ -65,4 +120,4 @@ def main():
     print('Main process exiting')
 
 
-main()
+thread()
